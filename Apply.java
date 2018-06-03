cb738cb547b34b4f4a64e44cc344aecaf30f984a
Pinokcio <mryu12@naver.com>
2018-06-03 16:44:47 +0900

    Add files via upload

---
+++ Apply.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Apply extends MouseAdapter {
+	private String word[];
+	public void mouseClicked(MouseEvent e) {
+		MNode.nodeArray.clear();
+		splitText();
+		
+		if(word == null) {
+			JOptionPane.showMessageDialog(null, "노드 입력하셈", "빈 노드 오류", JOptionPane.ERROR_MESSAGE);
+		}
+		else {
+			MNode.nodeArray.clear();
+			for(int i = 0; i < word.length; i++) {
+				MNode node = new MNode(word[i]);
+			}
+			new Draw();
+			MNode.print();
+		}
+	}
+	public void splitText() {
+		String fullText = Frame.textField.getText();
+		word = fullText.split("\n");
+	}
+}
--- MNode.java
+++ MNode.java
@@ -12,41 +12,58 @@ public class MNode {
 	private MNode parent = null;
 	private MNode firstChild = null;
 	private MNode nextSibling = null;
-	   
+	
 	public MNode (String text) {
 		for(int i = 0; i < text.length(); i++) {
 			if(text.charAt(i) == '\t') {
-		            ++rank;
+				++rank;
 			}
-		    else
-		    	break;
-		   	}
-		    if(rank == 0) {
-		    	parent = null;
-		      }
-		    else {
-		    	parent = recentParent.get(rank-1);
-		    	linkFirstChild();
-		    	linkNextSibling();
-		    }
-		    recentParent.add(rank, this);
-		    this.text = text.substring(rank);
-		    
-		    nodeArray.add(this);
-		   	}
-	
-	public void randomPositon () {
-		
-	}
-	public static void splitText() {
-		String fullText = Frame.textField.getText();
-		String word[] = fullText.split("\n");
-		
-		for(int i = 0; i < word.length; i++) {
-			MNode node = new MNode(word[i]);
-			nodeArray.add(i, node);
-			}
+			else
+				break;
 		}
+		if(rank == 0) {
+			parent = null;
+		}
+		else {
+			parent = recentParent.get(rank-1);
+			linkFirstChild();
+			linkNextSibling();
+		}
+		recentParent.add(rank, this);
+		this.text = text.substring(rank);
+		
+		nodeArray.add(this);
+	}
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public int getWidth() {
+		return this.width;
+	}
+	public int getHeight() {
+		return this.height;
+	}
+	public String getText() {
+		return this.text;
+	}
+	public String getColor() {
+		return this.color;
+	}
+	public int getRank() {
+		return this.rank;
+	}
+	public MNode getParent() {
+		return this.parent;
+	}
+	public MNode getFirstChild() {
+		return this.firstChild;
+	}
+	public MNode getNextSibling() {
+		return this.nextSibling;
+	}
 	public void linkFirstChild() {
 		//지금 노드의 rank-1 == 마지막arr원소의 rank 
 		MNode lastElem = nodeArray.get(nodeArray.size() - 1);
@@ -62,34 +79,21 @@ public class MNode {
 			}
 		}
 	}
-	public MNode getFirstChild() {
-		return this.firstChild;
+	public void setPositon() {
+		
 	}
-	public MNode getNextSibling() {
-		return this.nextSibling;
-	}
-	public int getRank() {
-		return this.rank;
+	//테스트용 출력함수
+	public static void print() {
+		for(int i = 0; i < nodeArray.size(); i++) {
+			System.out.println(nodeArray.get(i).getRank()+"순위 "+nodeArray.get(i).getText());
+			if(nodeArray.get(i).getParent() != null)
+				System.out.println("부모 : "+nodeArray.get(i).getParent().getText());
+			if(nodeArray.get(i).getFirstChild() != null)
+				System.out.println("첫자식 : "+nodeArray.get(i).getFirstChild().getText());
+			if(nodeArray.get(i).getNextSibling() != null)
+				System.out.println("다음형제 : "+nodeArray.get(i).getNextSibling().getText());
+			System.out.println("-------------");
 		}
-	public MNode getParent() {
-			return this.parent;
 	}
-	public String getText() {
-		return this.text;
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-	public int getWidth() {
-		return width;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public String getColor() {
-		return color;
-	}	
+	
 }
