e0df47f9413674c16fea8f9f237aa8bfea87f640
JeongAYoo <jeonga038@naver.com>
2018-06-04 12:32:45 +0900

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,52 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Frame extends JFrame{
 
-	private Container contentPane;
+	private static Container contentPane;
 	public static JTextArea textField = new JTextArea();
 	public static JPanel drawField = new JPanel();
 	public static JPanel AttributePane = new JPanel();
 	
-	private JSplitPane rootSplitPane = new JSplitPane();
+	private static JSplitPane rootSplitPane = new JSplitPane();
 	private static JSplitPane subRootRight = new JSplitPane();
-	private JSplitPane subRootLeft = new JSplitPane();
-	private JScrollPane textScroll = new JScrollPane(textField);
+	private static JSplitPane subRootLeft = new JSplitPane();
+	private static JScrollPane textScroll = new JScrollPane(textField);
 	private static JScrollPane mindScroll = new JScrollPane(drawField);
-	private JSplitPane sub_subRootRight = new JSplitPane();
-	private JScrollPane attributeScroll = new JScrollPane(AttributePane);
+	private static JSplitPane sub_subRootRight = new JSplitPane();
+	private static JScrollPane attributeScroll = new JScrollPane(AttributePane);
+	
+	//Dimension size = this.getBounds().getSize();
+	public static int frameWidth = 1600;
+	public static int frameHeight = 900;
+	
+	//AttributePane의 속성 값
+	public static JTextField textTF = new JTextField(20); 
+	public static JTextField xTF = new JTextField(20);
+	public static JTextField yTF = new JTextField(20);
+	public static JTextField wTF = new JTextField(20);
+	public static JTextField hTF = new JTextField(20);
+	public static JTextField colorTF = new JTextField(20);
 
+	
 	public Frame() {
 		setTitle("프레임");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(frameWidth, frameHeight);
 		contentPane = getContentPane();
 		Menu();
 		ToolBar();
 		splitPane();
-		setSize(900,600);
+		this.addComponentListener(new ResizeListener());
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	public void Menu() {
 		JMenuBar mb = new JMenuBar();
 		
-		JMenu newMenu = new JMenu("새로 만들기");
-		JMenu openMenu = new JMenu("열기");
-		JMenu saveMenu = new JMenu("저장");
-		JMenu saveAsMenu = new JMenu("다른 이름으로 저장");
-		JMenu exitMenu = new JMenu("닫기");
-		JMenu applyMenu = new JMenu("적용");
-		JMenu changeMenu = new JMenu("변경");
+		JMenu fileMenu = new JMenu("File");
+		JMenu saveAndChangeMenu = new JMenu("Save & Change");
 
-				
-		mb.add(newMenu);
-		mb.add(openMenu);
-		mb.add(saveMenu);
-		mb.add(saveAsMenu);
-		mb.add(exitMenu);
-		mb.add(applyMenu);
-		mb.add(changeMenu);
+		JMenuItem newItem = new JMenuItem("New");
+		JMenuItem openItem = new JMenuItem("Open");
+		JMenuItem saveItem = new JMenuItem("Save");
+		JMenuItem saveAsItem = new JMenuItem("SaveAs");
+		JMenuItem closeItem = new JMenuItem("Close");
+		JMenuItem applyItem = new JMenuItem("Apply");
+		JMenuItem changeItem = new JMenuItem("Change");
 		
+		openItem.addActionListener(new OpenActionListener());
+		saveItem.addActionListener(new SaveActionListener());
+		saveAsItem.addActionListener(new SaveAsActionListener());
+		applyItem.addActionListener(new ApplyActionListener());
+		closeItem.addActionListener(new CloseActionListener());
+		
+		fileMenu.add(newItem);
+		fileMenu.add(openItem);
+		fileMenu.add(closeItem);
+		saveAndChangeMenu.add(saveItem);
+		saveAndChangeMenu.add(saveAsItem);
+		saveAndChangeMenu.add(applyItem);
+		saveAndChangeMenu.add(changeItem);
+		
+		mb.add(fileMenu);
+		mb.add(saveAndChangeMenu);
+
 		setJMenuBar(mb);
 		setVisible(true);
 	}
@@ -54,47 +79,56 @@ public class Frame extends JFrame{
 		JToolBar toolBar = new JToolBar("Tool Bar");
 		toolBar.setFloatable(true);
 		
-		toolBar.add(new JButton("새로 만들기"));
-		toolBar.add(new JButton("열기"));
-		toolBar.add(new JButton("저장"));
-		toolBar.add(new JButton("다른 이름으로 저장"));
-		toolBar.add(new JButton("닫기"));
-		toolBar.add(new JButton("적용"));
-		toolBar.add(new JButton("변경"));
-
-		toolBar.addSeparator();
+		JButton newButton = new JButton("New");
+		JButton openButton = new JButton("Open");
+		JButton saveButton = new JButton("Save");
+		JButton saveAsButton = new JButton("SaveAs");
+		JButton closeButton = new JButton("Close");
+		JButton applyButton = new JButton("Apply");
+		JButton changeButton = new JButton("Change");
 		
-		JComboBox<String> combo = new JComboBox<String>();
-		combo.addItem("Java");
-		combo.addItem("C#");
-		combo.addItem("C");
-		combo.addItem("C++");
-		toolBar.add(combo);
+		toolBar.add(newButton);
+		toolBar.add(openButton);
+		toolBar.add(saveButton);
+		toolBar.add(saveAsButton);
+		toolBar.add(closeButton);
+		toolBar.add(applyButton);
+		toolBar.add(changeButton);
+		
+		openButton.addActionListener(new OpenActionListener());
+		saveButton.addActionListener(new SaveActionListener());
+		saveAsButton.addActionListener(new SaveAsActionListener());
+		applyButton.addActionListener(new ApplyActionListener());
+		closeButton.addActionListener(new CloseActionListener());
+		
+		toolBar.addSeparator();
 		
 		contentPane.add(toolBar, BorderLayout.NORTH);
 	}
 	public void splitPane() {
 		//왼쪽
+		textField.setFont(new Font("",Font.PLAIN, 30));
+		textField.setTabSize(3);
 		subRootLeft.setTopComponent(textScroll);
 
-		JButton apply = new JButton("적용");
-		apply.setBackground(Color.PINK);
-		apply.addMouseListener(new Apply());
+		JButton apply = new JButton("APPLY");
+		apply.setFont(new Font("Arial", Font.BOLD, 20));
+		apply.setBackground(Color.ORANGE);
+		apply.addActionListener(new ApplyActionListener());
 		subRootLeft.setBottomComponent(apply);
 		
 		//오른쪽
 		sub_subRootRight.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		sub_subRootRight.setDividerLocation(450);
-		sub_subRootRight.setDividerSize(0);
+
 		AttributePane.setBackground(Color.WHITE);;
 		sub_subRootRight.setTopComponent(attributeScroll);
-		JButton change = new JButton("변경");
-		change.setBackground(Color.PINK);
+		JButton change = new JButton("CHANGE");
+		change.setFont(new Font("Arial", Font.BOLD, 20));
+		change.setBackground(Color.ORANGE);
 		sub_subRootRight.setBottomComponent(change);
-		
+				
 		subRootRight.setRightComponent(sub_subRootRight);
-		//가운데
-		drawMind();
+		sub_subRootRight.setDividerSize(0);
 		
 		rootSplitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
 		subRootRight.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
@@ -102,8 +136,12 @@ public class Frame extends JFrame{
 		subRootLeft.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		rootSplitPane.setLeftComponent(subRootLeft);
 		
-		rootSplitPane.setDividerLocation(270);
-		subRootLeft.setDividerLocation(450);
+
+		//가운데
+		drawMind();
+		//locateDivider();
+		drawAttribute();
+
 		subRootLeft.setDividerSize(0);
 		
 		this.contentPane.add(rootSplitPane);
@@ -112,6 +150,44 @@ public class Frame extends JFrame{
 		drawField.setLayout(null);
 		drawField.setBackground(Color.WHITE);
 		subRootRight.setLeftComponent(mindScroll);
-		subRootRight.setDividerLocation(300);
+		
+		locateDivider();
+	}
+	public static void locateDivider() {
+		subRootRight.setDividerLocation((int)(frameWidth*(2/4.0)));
+		sub_subRootRight.setDividerLocation(frameHeight - 180);		//버튼크기는 항상 같게
+		subRootLeft.setDividerLocation(frameHeight - 180);
+		rootSplitPane.setDividerLocation((int)(frameWidth*(1/4.0)));
+	}
+	public void drawAttribute() {
+		Font font = new Font("", Font.PLAIN, 20);
+		AttributePane.setLayout(new GridLayout(0, 2));
+		JLabel textLB = new JLabel("TEXT : ");
+		JLabel xLB = new JLabel("X : ");
+		JLabel yLB = new JLabel("Y : ");
+		JLabel wLB = new JLabel("W : ");
+		JLabel hLB = new JLabel("H : ");
+		JLabel colorLB = new JLabel("Color : ");
+		textLB.setFont(font);
+		xLB.setFont(font);;
+		yLB.setFont(font);
+		wLB.setFont(font);
+		hLB.setFont(font);
+		colorLB.setFont(font);
+		
+		AttributePane.add(textLB);
+		textTF.setEditable(false);
+		AttributePane.add(textTF);
+		AttributePane.add(xLB);
+		AttributePane.add(xTF);
+		AttributePane.add(yLB);
+		AttributePane.add(yTF);
+		AttributePane.add(wLB);
+		AttributePane.add(wTF);
+		AttributePane.add(hLB);
+		AttributePane.add(hTF);
+		AttributePane.add(colorLB);
+		AttributePane.add(colorTF);
+
 	}
 }
